71d0fb6a3650a2780743abdc4df8bd014609b21c
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-01-05 12:31:18 -0600

    replaced downloads table with card deck

--- src/pages/DownloadsPage.tsx
+++ src/pages/DownloadsPage.tsx
@@ -1,11 +1,20 @@
 import React, { FC, useState, useEffect } from 'react';
-import { Container, Table, Spinner } from 'reactstrap';
+import {
+  Container,
+  Card,
+  CardDeck,
+  CardBody,
+  CardTitle,
+  CardFooter,
+  CardLink,
+  Spinner
+} from 'reactstrap';
 
 type DownloadsData = {
   filename?: string;
   url: string;
   comments?: string;
-  uploaded_on?: Date;
+  uploaded?: Date;
 };
 
 export const DownloadsPage: FC = () => {
@@ -15,36 +24,32 @@ export const DownloadsPage: FC = () => {
       setData(await (await fetch('https://api.bharatmiddha.com/rest/downloads')).json());
     fetchData();
   }, []);
-  console.log('data', data);
   return (
     <Container>
       <h1 className="my-4 wack">Downloads</h1>
       {data ? (
-        <>
-          <Table>
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Filename</th>
-                <th scope="col">URL</th>
-                <th scope="col">Comments</th>
-                <th scope="col">Uploaded on</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((e, key) => (
-                <tr key={key}>
-                  <td>{key + 1}</td>
-                  <td>
-                    <a href={e.url}>{e.filename ? e.filename : e.url}</a>
-                  </td>
-                  <td>{e.comments}</td>
-                  <td>{e.uploaded_on?.toLocaleDateString()}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </>
+        <CardDeck>
+          {data.map((e, key) => (
+            <div>
+              <Card
+                key={key}
+                className="mt-4"
+                style={{ width: '250px', height: 'calc(100% - 30px)' }}
+              >
+                <CardBody>
+                  <CardTitle>{e.filename ? e.filename : e.url}</CardTitle>
+                  <p>{e.comments}</p>
+                  <CardLink href={e.url} target="_blank" rel="noopener noreferrer">
+                    {e.url}
+                  </CardLink>
+                </CardBody>
+                <CardFooter className="text-muted">
+                  {e.uploaded ? `Uploaded on ${new Date(e.uploaded).toLocaleDateString()}` : ' '}
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
+        </CardDeck>
       ) : (
         <div className="d-flex align-items-center">
           <Spinner className="mr-2" type="grow" />
