c972308c8de13aff3b183272c4ffe60565b4382e
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2021-06-18 10:54:07 -0500

    prettier printWidth 80

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
