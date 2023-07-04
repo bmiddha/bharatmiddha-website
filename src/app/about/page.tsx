9e0aebc550bed3f1bfb7c0694c7bf57c83e509ca
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2023-07-04 11:37:19 -0700

    tailwindcss prettier plugin

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function Posts() {
+  return <h1>About</h1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-24">
-      <div className="z-10 w-full max-w-5xl items-center flex-col text-center font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-24 p-24">
+      <div className="z-10 w-full max-w-5xl flex-col items-center text-center font-mono text-sm lg:flex">
         {/* round image */}
-        <div className="relative w-32 h-32 overflow-hidden rounded-full m-auto">
+        <div className="relative m-auto h-32 w-32 overflow-hidden rounded-full">
           <Image src="/penguin-avatar.jpg" alt="" role="presentation" height={300} width={300} />
         </div>
-        <h1 className='text-2xl mb-4'>Hi internet! I&apos;m Bharat.</h1>
+        <h1 className="mb-4 text-2xl">Hi internet! I&apos;m Bharat.</h1>
         <p>you found me :&#41;</p>
       </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-1 lg:text-left flex-col">
+      <div className="mb-32 grid flex-col text-center lg:mb-0 lg:grid-cols-1 lg:text-left">
         <a
           href="https://github.com/bmiddha"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
